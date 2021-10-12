9f70ed64bb619a592f837f27a77d6bbbeaab531f
yangsw3114 <72145880+yangsw3114@users.noreply.github.com>
2021-10-12 17:23:41 +0900

    commit#2

--- docs/mysite.sql
+++ docs/mysite.sql
@@ -2,3 +2,8 @@
 
 desc user;
 
+insert into user values(null, '둘리','dooly@gmail.com', '1234','male',now());
+
+select * from user;
+
+select no, name from user where email='kickscar@gmail.com' and password='1234';
