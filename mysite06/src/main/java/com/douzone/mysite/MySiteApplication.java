0d47fa98c8366cd84dfeedd1fb53e2921a84fbb6
yangsw3114 <72145880+yangsw3114@users.noreply.github.com>
2021-11-02 12:02:18 +0900

    mysite06

--- mysite06/src/main/java/com/douzone/mysite/MySiteApplication.java
+++ mysite06/src/main/java/com/douzone/mysite/MySiteApplication.java
@@ -1,5 +1,11 @@
 package com.douzone.mysite;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class MySiteApplication{
+	public static void main(String[] args) {
+		SpringApplication.run(MySiteApplication.class, args);
+	}
 }
--- mysite06/src/main/java/com/douzone/mysite/config/WebConfig.java
+++ mysite06/src/main/java/com/douzone/mysite/config/WebConfig.java
@@ -1,37 +1,142 @@
 package com.douzone.mysite.config;
 
+import java.nio.charset.Charset;
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
-import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import com.douzone.config.web.FileUploadConfig;
-import com.douzone.config.web.MessageConfig;
-import com.douzone.config.web.MvcConfig;
-import com.douzone.config.web.SecurityConfig;
 import com.douzone.mysite.interceptor.SiteInterceptor;
+import com.douzone.mysite.security.AuthInterceptor;
+import com.douzone.mysite.security.AuthUserHandlerMethodArgumentResolver;
+import com.douzone.mysite.security.LoginInterceptor;
+import com.douzone.mysite.security.LogoutInterceptor;
 
 
 @Configuration
-@EnableAspectJAutoProxy
-@ComponentScan({"com.douzone.mysite.controller", "com.douzone.mysite.exception"})
-@Import({MvcConfig.class, MessageConfig.class, FileUploadConfig.class, SecurityConfig.class})
-public class WebConfig extends WebMvcConfigurerAdapter {
+@PropertySource("classpath:com/douzone/mysite/config/WebConfig.properties")
+public class WebConfig implements WebMvcConfigurer {
 	
-	// SiteInterceptor
+	@Autowired
+	private Environment env;
+
+	// Argument Resolver
+	@Bean
+	public HandlerMethodArgumentResolver handlerMethodArgumentResolver() {
+		return new AuthUserHandlerMethodArgumentResolver();
+	}
+	
+	@Override
+	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
+		argumentResolvers.add(handlerMethodArgumentResolver());
+	}
+	
+	
+	// Interceptors
 	@Bean
 	public HandlerInterceptor siteInterceptor() {
 		return new SiteInterceptor();
 	}
 	
+	@Bean
+	public HandlerInterceptor loginInterceptor() {
+		return new LoginInterceptor();
+	}
+
+	@Bean
+	public HandlerInterceptor logoutInterceptor() {
+		return new LogoutInterceptor();
+	}
+
+	@Bean
+	public HandlerInterceptor authInterceptor() {
+		return new AuthInterceptor();
+	}
+	
 	@Override
 	public void addInterceptors(InterceptorRegistry registry) {
+		
 		registry
-			.addInterceptor(siteInterceptor())
-			.addPathPatterns("/**");
-	}	
+		.addInterceptor(siteInterceptor())
+		.addPathPatterns("/**");
+	
+		registry
+			.addInterceptor(loginInterceptor())
+			.addPathPatterns("/user/auth");
+		
+		registry
+			.addInterceptor(logoutInterceptor())
+			.addPathPatterns("/user/logout");
+		
+		registry
+			.addInterceptor(authInterceptor())
+			.addPathPatterns("/**")
+			.excludePathPatterns("/user/auth")
+			.excludePathPatterns("/user/logout")
+			.excludePathPatterns("/assets/**");		
+	}
+	
+	
+	
+	// Message Converter
+		@Bean
+		public StringHttpMessageConverter stringHttpMessageConverter() {
+			StringHttpMessageConverter messageConverter = new StringHttpMessageConverter();
+			messageConverter.setSupportedMediaTypes(
+				Arrays.asList(
+					new MediaType("text", "html", Charset.forName("utf-8")))
+				);
+
+			return messageConverter;
+		}
+		
+		@Bean
+		public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+			Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder()
+				.indentOutput(true)
+				.dateFormat(new SimpleDateFormat("yyyy-mm-dd"));
+			
+			MappingJackson2HttpMessageConverter messageConverter = new MappingJackson2HttpMessageConverter(builder.build());
+			messageConverter.setSupportedMediaTypes(
+				Arrays.asList(
+					new MediaType("application", "json", Charset.forName("utf-8"))
+				)
+			);
+			
+			return messageConverter;
+		}
+		
+		@Override
+		public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+			converters.add(stringHttpMessageConverter());
+			converters.add(mappingJackson2HttpMessageConverter());
+		}
+	
+
+		//Resource Mapping(URL Magic Mapping)
+		@Override
+		public void addResourceHandlers(ResourceHandlerRegistry registry) {
+			registry
+				.addResourceHandler(env.getProperty("fileupload.resourceMapping"))
+				.addResourceLocations("file:" + env.getProperty("fileupload.uploadLocation"));
+		}
+		
+		
 }
